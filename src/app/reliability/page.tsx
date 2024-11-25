0cf84bcfb23a43d83ca643a647dec6772333e0a8
WilSonLibs <phiriwilson977@gmail.com>
2024-11-25 22:09:15 +0200

    just a  commit

---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,10 @@
+import reliabilityImg from 'public/reliability.jpg';
+import Hero from '@/components/hero';
+export default function ReliabilityPage() {
+  return  ( 
+     
+    <Hero imgData={reliabilityImg}
+     imgAlt='welding' 
+     title='We Are Glad You Came '/>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function Heaader(){
+return(<div className='w-full absolute text-white z-10'>
+       <nav className='container relative flex flex-wrap items-cenetr justify-between mx-auto p-8'> 
+        <Link className='font-bold text-3xl' href="/">Home</Link>
+        <div className='space-x-4 text-xl'>
+
+        <Link href="/performance">Performance</Link>
+      <Link href="/scale">Scale</Link>
+      <Link href="/reliability">Reliability</Link>
+        </div>
+      </nav>
+        </div>);
+}
